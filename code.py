5a3db3ded8e8fc209c0795ad7f8cf66355334138
Maxim <maxonion@protonmail.com>
2020-09-17 16:44:49 +0000

    Printing info about group

---
+++ code.py
@@ -0,0 +1,11 @@
+import vk
+
+accessFile = open("access.key", "r")
+accessToken = accessFile.read()
+accessFile.close()
+
+groupId = input()
+
+session = vk.Session(access_token=accessToken)
+api = vk.API(session, v='5.124', lang='ru', timeout=10)
+print(api.groups.getById(group_id=groupId))
