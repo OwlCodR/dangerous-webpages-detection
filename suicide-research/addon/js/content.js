f9d7e0b9fa48d6077a9ce82b0f7e57143fe9baf1
unknown <owlcodr>
2020-10-04 16:21:22 +0300

    Added python searching

--- suicide-research/addon/js/content.js
+++
@@ -1,7 +0,0 @@
-chrome.runtime.onMessage.addListener(
-    function (request, sender, sendResponse) {
-        if (request.method == "getText") {
-            sendResponse({data: document.body.outerHTML, method: "getText"});
-        }
-    }
-);
